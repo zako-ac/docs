47e2260768def517f2b1d9da68d6a4acae84884c
minco <mail@drchi.co.kr>
2026-01-07 23:08:07 +0900

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import { Roboto, Noto_Sans_KR } from 'next/font/google';
+import clsx from 'clsx';
 
-const inter = Inter({
-  subsets: ['latin'],
+const roboto = Roboto({
+    subsets: ['latin'],
 });
 
+const notoSans = Noto_Sans_KR();
+
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="ko" className={clsx(roboto.className, notoSans.className)} suppressHydrationWarning>
+            <body className="flex flex-col min-h-screen">
+                <RootProvider>{children}</RootProvider>
+            </body>
+        </html>
+    );
 }
